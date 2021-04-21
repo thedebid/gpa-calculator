84670a50f34ecc2257e8b7d232f04bfde2c12335
Debid Magar <linkwithdm@gmail.com>
2021-04-21 21:30:05 +0545

    unique semester only added to semeser list done

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,20 +1,18 @@
 //Reducer => a function that takes in the old state, and an action => return new state
-import gpaData from "./../data/GPA.json";
 
 const contextReducer = (state, action) => {
   // console.log(gpaData);
   switch (action.type) {
-    case "REMOVE_COURSE":
-      gpaData = state.filter((t) => t.id === action.payload);
-      return gpaData;
-    case "ADD_TRANSACTION":
-      gpaData = [action.payload, ...state];
-      return gpaData;
     case "ADD_SEMESTER":
-      const semester = { name: action.payload, courses: [] };
       var semesters = [...state];
-      semesters.push(semester);
-      return semesters;
+      const status = semesters.find((e) => e.name === action.payload);
+      if (status === undefined) {
+        const semester = { name: action.payload, courses: [] };
+        semesters.push(semester);
+        return semesters;
+      } else {
+        return state;
+      }
     case "REMOVE_SEMESTER":
       const index = state.indexOf(action.payload);
       var semesters = [...state];
